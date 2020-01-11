d685ec800e0424441d21fff0d82b394fdf1d0dc4
Mike Thurgood <mikecthurgood@gmail.com>
2020-01-11 12:01:57 +0000

    routes and wall view functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+import API from './helpers/API'
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import NavBar from './components/NavBar'
+import MainContainer from './components/MainContainer'
 
-function App() {
+const App = () => {
+
+  const [user, setUser] = useState({ username: null })
+  const [walls, setWalls] = useState([])
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    token ?
+      API.validate()
+        .then(data => {
+          if (data.error) throw Error(data.error)
+          localStorage.setItem('token', data.token)
+          setUser({ username: data.user.username })
+          console.log(data.user)
+        })
+        .catch(error => {
+          console.log(error)
+          localStorage.removeItem('token')
+        })
+      :
+      console.log('no token stored')
+  }, [])
+
+  const signIn = userData => {
+    localStorage.setItem('token', userData.token)
+    setUser({ username: userData.user.username })
+  }
+
+  const signInTest = () => API.signIn('MikeTest', 'm').then(data => signIn(data))
+
+  const signOut = () => {
+    setUser({})
+    localStorage.removeItem('token')
+  }
+
+  // code for throwing error if user not found for JS side
+  //   .then(data => {
+  //     if (data.error) throw Error(data.error)
+  //   })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar
+        signInTest={signInTest}
+        signOut={signOut}
+        user={user}
+      />
+      <MainContainer
+        walls={walls}
+        setWalls={setWalls}
+      />
+    </>
   );
 }
 
-export default App;
+export default withRouter(App)
