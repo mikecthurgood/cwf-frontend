e0bd46b402d1e24f7f969ba600861793c5f02b18
Mike Thurgood <mike.thurgood@wintechnologies.net>
2020-08-19 11:55:19 +0100

    update endpoint

--- src/helpers/API.js
+++ src/helpers/API.js
@@ -1,5 +1,5 @@
 const get = (graphqlQuery) => (
-    fetch(`${process.env.ENDPOINT}/graphql`, {
+    fetch(`http://${process.env.ENDPOINT}/graphql`, {
         method: 'POST',
         body: JSON.stringify(graphqlQuery),
         headers: {
@@ -11,7 +11,7 @@ const get = (graphqlQuery) => (
 )
 
 const post = (graphqlQuery, token) =>(
-    fetch(`${process.env.ENDPOINT}/graphql`, {
+    fetch(`http://${process.env.ENDPOINT}/graphql`, {
         method: 'POST',
         body: JSON.stringify(graphqlQuery),
         headers: {
--- src/helpers/Auth.js
+++ src/helpers/Auth.js
@@ -21,7 +21,7 @@ export const handleLogin = (authData) => {
             password: password
           }
         }
-        fetch(`${process.env.ENDPOINT}/graphql`, {
+        fetch(`http://${process.env.ENDPOINT}/graphql`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
@@ -94,7 +94,7 @@ export const handleLogin = (authData) => {
             password: password
         }
         }
-        const resData = await fetch(`${process.env.ENDPOINT}/graphql`, {
+        const resData = await fetch(`http://${process.env.ENDPOINT}/graphql`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
