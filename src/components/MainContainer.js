836b7cfd4d83e4bf757a6d81d152e70cbab65071
mikecthurgood <mikecthurgood@gmail.com>
2021-01-09 21:23:52 +0000

    update to context api

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,43 +1,41 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useContext } from 'react'
 import { Route, Switch } from 'react-router-dom';
+import Store from '../context/Store';
 
 const Home = React.lazy(() => import('../pages/Home'))
 const AuthForm = React.lazy(() => import('./form/AuthForm'))
 const SingleWall = React.lazy(() => import('../pages/singleWall'));
 const ErrorPage = React.lazy(() => import('../pages/ErrorPage'));
 
-const MainContainer = ({ filterSelection, scrollPosition, setScrollPosition, setSignUpSuccess, signUpSuccess, setFilterSelection, walls, setWalls, user, setUserPostCode, signupHandler, signOut,  userPostCode, searchFilter }) => (
-    <div className='main-container'>
-        <Switch>
-            <Route exact path="/" render={(routerProps) => <Suspense fallback={<div>Loading...</div>}><Home
-                {...routerProps}
-                filterSelection={filterSelection}
-                scrollPosition={scrollPosition}
-                setScrollPosition={setScrollPosition}
-                setWalls={setWalls}
-                searchFilter={searchFilter}
-                signOut={signOut}
-                user={user}
-                walls={walls}
-                userPostCode={userPostCode} 
-                setFilterSelection={setFilterSelection}
-                setUserPostCode={setUserPostCode}
-                setSignUpSuccess={setSignUpSuccess}
-                signUpSuccess={signUpSuccess}
-            /></Suspense>} />
-            <Route exact path="/signup" render={(routerProps) => <Suspense fallback={<div>Loading...</div>}>
-                <AuthForm {...routerProps} 
-                    onSubmit={signupHandler} 
-                    signup={true} 
-                    user={user} 
-                    loginToggle={() => {}}
-                    loginMenuToggle={() => {}}
-                />
-            </Suspense>}/>
-            <Route exact path="/walls/:wallSlug" render={(routerProps) => <Suspense fallback={<div>Loading...</div>}><SingleWall {...routerProps} signOut={signOut} user={user} /> </Suspense>}/>
-            <Route path='*' render={(routerProps) => <Suspense fallback={<div>Loading...</div>}><ErrorPage /> </Suspense>}/>
-        </Switch>
-    </div>
-)
+const MainContainer = ({walls}) => {
+    const { signupHandler, user } = useContext(Store)
+
+    return (
+        
+        <div className='main-container'>
+            <Switch>
+                <Route exact path="/" render={(routerProps) => <Suspense fallback={<div>Loading...</div>}>
+                    <Home {...routerProps} walls={walls} />
+                </Suspense>} />
+                <Route exact path="/signup" render={(routerProps) => <Suspense fallback={<div>Loading...</div>}>
+                    <AuthForm {...routerProps} 
+                        onSubmit={signupHandler} 
+                        signup={true} 
+                        user={user} 
+                        loginToggle={() => {}}
+                        loginMenuToggle={() => {}}
+                    />
+                </Suspense>}/>
+                <Route exact path="/walls/:wallSlug" render={(routerProps) => <Suspense fallback={<div>Loading...</div>}>
+                    <SingleWall {...routerProps} /> 
+                </Suspense>}/>
+                <Route path='*' render={(routerProps) => <Suspense fallback={<div>Loading...</div>}>
+                    <ErrorPage />
+                </Suspense>}/>
+            </Switch>
+        </div>
+    )
+}
+
 
 export default MainContainer
