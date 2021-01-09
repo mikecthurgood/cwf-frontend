a9ec765d8db315644366edb190092c55cf4167b9
mikecthurgood <mikecthurgood@gmail.com>
2021-01-09 21:24:52 +0000

    update to context api

--- src/components/sortAndFilter/PostcodeSort.js
+++ src/components/sortAndFilter/PostcodeSort.js
@@ -48,48 +48,6 @@ const PostcodeSort = ({clearPostcode, postCodeInput, handlePostCodeChange, handl
                     <input type="submit" value='Set' className='submit-postcode' />
                 </form>
             }
-            {/* <form>
-                <label className='filter__menu-checkbox'>
-                    Bouldering: 
-                    <input
-                        name="bouldering"
-                        type="checkbox"
-                        checked={filterSelection.bouldering}
-                        onChange={handleFilterChange} 
-                    />
-                    <span className="checkbox"></span>
-                </label>
-                <label className='filter__menu-checkbox'>
-                    Top Roping: 
-                    <input
-                        name="top"
-                        type="checkbox"
-                        checked={filterSelection.top}
-                        onChange={handleFilterChange} 
-                    />
-                    <span className="checkbox"></span>
-                </label>
-                <label className='filter__menu-checkbox'>
-                    Lead: 
-                    <input
-                        name="lead"
-                        type="checkbox"
-                        checked={filterSelection.lead}
-                        onChange={handleFilterChange} 
-                    />
-                    <span className="checkbox"></span>
-                </label>
-                <label className='filter__menu-checkbox'>
-                    Auto Belay: 
-                    <input
-                        name="auto"
-                        type="checkbox"
-                        checked={filterSelection.auto}
-                        onChange={handleFilterChange} 
-                    />
-                    <span className="checkbox"></span>
-                </label>
-            </form> */}
         </>
     )
 }
